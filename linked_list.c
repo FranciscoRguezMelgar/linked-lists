cf2c9ca1156c485fc377f6d648487cda88e472b9
FranciscoRguezMelgar <franciscorguezmelgar@gmail.com>
2016-02-22 17:08:14 +0100

    Added files via upload

--- linked_list.c
+++ linked_list.c
@@ -2,193 +2,136 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-linked_list* create(int size){
-	linked_list* list = (linked_list*) malloc(sizeof(linked_list));
-	list->next = NULL;
-	list->content = malloc(size);
-	bzero(list->content, size);
-	list->size = size;
-	return list;
-}
-int open_up(int size, void* element){
-	linked_list* list = (linked_list*) malloc(sizeof(linked_list));
-	list->next = NULL;
-	list->content = element;
-	bzero(list->content, size);
-	list->size = size;
-	return 0;
-}
-int insert_at(int index , void* element, linked_list* list){
-	int ii=0;
-	int size=0;
-	linked_node* current_node = (linked_node*)malloc(sizeof(linked_node));
-	linked_node* new_node = (linked_node*)malloc(sizeof(linked_node));
-	linked_list* newList = (linked_list*)malloc(sizeof(linked_list));;
-	if(index > 1){
-		if(get_size(list)!=0){
-			size = list->size;
-		}
-		else{
-			printf("Lista no inicializada\n");
-			return -1;
-		}
-		for (ii = 0; ii < index-1; ++ii){ /*NÓTESE QUE LA CONDICIÓN HA CAMBIADO (ii < index-1)*/
-			if(ii==0&&list->next!=NULL) current_node = list->next;
-			else if(current_node->next!=NULL){			
-				current_node=current_node->next;
-			}	
-			else{
-				printf("El índice está fuera de rango.\n");		
-				return -1 ;/*Código de índice incorrecto, demasiado bajo o demasiado alto.*/
-			}
-		}
-		new_node->next = current_node->next;
-		new_node->content = element;
-		current_node->next=new_node;
-		return 0;
-	}else if(index==0){
-		if(get_size(list)!=0){
-			new_node->content = list->content;
-			new_node->next = list->next;
-			list->content = element;
-			list->next = new_node;			
-		}else{
-			printf("Lista no inicializada.\n");
-			return -1;
-		}
-		return 0;
-	}else if(index==1){
-		new_node->next = list->next;
-		new_node->content = element;
-		list->next = new_node;
-	}	
-}
-int del_at(int index, linked_list* list){
-	int ii=0;
-	int size=0;
-	linked_node* current_node = (linked_node*)malloc(sizeof(linked_node));
-	if(index > 1){
-		if(get_size(list)!=0){
-			size = list->size;
-		}
-		else{
-			printf("Lista no inicializada\n");
-			return -1;
-		}
-		for (ii = 0; ii < index-1; ++ii){ /*NÓTESE QUE LA CONDICIÓN HA CAMBIADO (ii < index-1)*/
-			if(ii==0&&list->next!=NULL) current_node = list->next;
-			else if(current_node->next!=NULL){			
-				current_node=current_node->next;
-			}	
-			else{
-				printf("El índice está fuera de rango.\n");		
-				return -1 ;/*Código de índice incorrecto, demasiado bajo o demasiado alto.*/
-			}
-		}
-		linked_node* trash;
-		trash = current_node->next;
-		current_node->next=current_node->next->next;
-		free(trash);
-		return 0;
-	}else if(index==0){
-		if(get_size(list)>1){
-			linked_node* trash;
-			list->content = list->next->content;
-			trash = list->next;
-			list->next = list->next->next;
-			free(trash);
-			return 0;
-		}else if(get_size(list)==1){
-			free(list);
-			printf("\tSe ha eliminado el último elemento de la lista, \n\tes necesario igualar list a NULL fuera del método\n");
-			return 1; //código de lista vacía, necesario igualar list a NULL fuera del método.
-		}else if(get_size(list)==0){
-			printf("Lista no inicializada.\n");
-			return -1;
-		}
-		return 0;
-	}else if(index==1){
-		if(get_size(list)<2){
-			printf("El índice está fuera de rango.\n");		
-			return -1;
-		}
-		linked_node* trash;
-		trash = list->next;
-		list->next = list->next->next;
-		free(trash);
-	}
-}
 
-int get_at (int index, 	void* element,  linked_list* list){
-	int ii=0;
-	int size=0;	
-	linked_node* current_node;
-	if(get_size(list)!=0){
-		size = list->size;
-	}
-	else{
-		printf("Lista no inicializada\n");
+linked_list* create(int size){
+	linked_list* lista = (linked_list*) malloc(sizeof(linked_list));
+	lista->size = size;
+	lista->next = NULL;
+}
+int insert_at(linked_list* list, int index , void* elemento){
+	if(elemento==NULL) return -1;
+	if(index < 0 || index > get_size(list)){
+		printf("Índice fuera de rango\n");
 		return -1;
 	}
-	if(index > 0){
-		for (ii = 0; ii < index; ++ii){
-			if(ii==0&&list->next!=NULL) current_node = list->next;
-			else if(current_node->next!=NULL){			
-				current_node=current_node->next;
-			}	
-			else{
-				printf("El índice está fuera de rango.\n");		
-				return -1 ;/*Código de índice incorrecto, demasiado bajo o demasiado alto.*/
-			}
-		}
-		memcpy(element, current_node->content, size);	
+	linked_node* new_node = (linked_node*) malloc(sizeof(linked_node));
+	int size = list->size;
+	int ii=0;
+	linked_node* current_node;
+	new_node->content = malloc(size);
+	if (index==0){
+		//***caso especial de insertar en la posición 0
+		new_node->next = list->next;
+		list->next = new_node;
+		memcpy(new_node->content, elemento, size);
+		return 0;
+	}//else //si la posición no es 0
+	current_node = list->next;
+	while(ii<index-1){
+		current_node = current_node->next;
+		ii++;
+	}
+	new_node->next = current_node->next;
+	current_node->next = new_node;
+	memcpy(new_node->content, elemento, size);
+	return 0;
+}
+int del_at(linked_list* list, int index){
+	int n_elements = get_size(list);
+	if(index < 0 || index > (n_elements - 1)){
+		printf("Índice fuera de rango\n");
+		return -1;
+	}
+	int size = list->size;
+	int ii=0;
+	linked_node* current_node;
+	if (index==0){
+		//***caso especial de borrar de la posición 0
+		free(list->next->content);
+		linked_node* trash = list->next;
+		list->next = list->next->next;
+		free(trash);
 		return 0;
 	}
-	memcpy(element, list->content, size);
+	current_node = list->next;
+	while(ii<index-1){
+		current_node = current_node->next;
+		ii++;
+	}
+	free(current_node->next->content);
+	linked_node* trash = current_node->next;
+	current_node->next = current_node->next->next;
+	free(trash);
+	return 0;
+}
+int get_at (linked_list* list, int index , void* elemento){
+	if(elemento==NULL) return -1;
+	int n_elements = get_size(list);
+	int ii = 0;
+	if(index < 0 || index > (n_elements - 1)){
+		printf("Índice fuera de rango\n");
+		return -1;
+	}
+	int size = list->size;
+	linked_node* current_node;
+	current_node = list->next;
+	while(ii<index){
+		current_node = current_node->next;
+		ii++;
+	}
+	memcpy(elemento, current_node->content, size);
+	return 0;
+}
+int owrite_at(linked_list* list, int index, void* elemento){
+	if(elemento==NULL) return -1;
+	int n_elements = get_size(list);
+	int ii = 0;
+	if(index < 0 || index > (n_elements - 1)){
+		printf("Índice fuera de rango\n");
+		return -1;
+	}
+	int size = list->size;
+	linked_node* current_node;
+	current_node = list->next;
+	while(ii<index){
+		current_node = current_node->next;
+		ii++;
+	}
+	memcpy(current_node->content, elemento, size);
 	return 0;
 }
 int get_size(linked_list* list){
+	int ii = 0;
+	linked_node* current_node;
 	if(list==NULL){
-		return 0;
-	}else if(list->next==NULL){
-		return 1;
-	}else{
-		linked_node* current_node;
-		int ii;
+		printf("\nLista no inicializada\n");
+		return -1;
+	}
+	if(list->next!=NULL){
+		ii = 1;
 		current_node = list->next;
-		for(ii=2;current_node->next!=NULL;ii++){		
-				current_node = current_node->next;
-		}
-		return ii;
+	}else{
+		return 0;
 	}
+	while(current_node->next!=NULL){
+		ii++;
+		current_node = current_node->next;
+	}
+	return ii;
 }
-
-int main(){
-	int i, res, tama;
-	char * pepe = (char*) malloc(sizeof(char)*50);
-
-	linked_list* list= create(sizeof(int));
-
-	for(i = 0; i< 4;i++){
-			insert_at(get_size(list), &i, list);
-	}
-	tama =get_size(list);
-	printf("El tamaño de la lista es: %d\n", tama);
-	for(i = 0; i< get_size(list); i++){
-		get_at(i, &res, list);
-		printf("en la posición %d hay un %d\n",i, res );
-	}
-	for(i=0;i < tama; i++){
-		if(del_at(0, list)==1){
-			list=NULL;
-		}
-		printf("El tamaño de la lista es: %d\n", get_size(list));	
-	}
-	printf("hoiuadhfkh\n");
-	list= create(sizeof(int));
-
-	for(i = 0; i< 4;i++){
-			insert_at(get_size(list), &i, list);
-	}
-
+int main(int argc, char** argv){
+	linked_list* list = create(sizeof(int));
+	int numero = 654;
+	int numero2 = 987;
+	int res = 0;
+	insert_at(list, 0,&numero);
+	insert_at(list, get_size(list), &numero2);
+	get_at(list,0, &numero2);
+	printf("En 'res' hemos puesto un %d\n", res);
+	printf("El tamaño de la lista es: %d\n", get_size(list));
+	del_at(list, get_size(list)-1);
+	del_at(list, 0);
+	printf("El tamaño de la lista es: %d\n", get_size(list));
+	free(list);
+	return 0;
 }
--- linked_list.h
+++ linked_list.h
@@ -2,7 +2,6 @@
 typedef struct struct_linked_list
 {	
 	int size;
-	void* content;
 	struct struct_linked_node* next;
 }linked_list;
 typedef struct struct_linked_node
@@ -12,15 +11,13 @@ typedef struct struct_linked_node
 }linked_node;
 //Inicia el primer nodo de la lista con la estructura a la que apunte inicializada a 0.
 linked_list* create(int size);
-//Inicializa la lista con un elemento dado
-int open_up(int size, void *element);
 //Inserta un elemento en la lista en la posición que indica index.
-int insert_at(int index , void* elemento, linked_list* list);
+int insert_at(linked_list* list, int index , void* elemento);
 //Elimina de la lista un elemento, pero no libera la memoria que éste ocupa.
-int del_at(int index, linked_list* list);
-//Elimina un elemento de la lista y libera la memoria que la estrcutura ocupa.
-int free_at(int index, linked_list* list);
+int del_at(linked_list* list, int index);
 //copia en la dirección apuntada por 'elemento' el elemento en la posición 'index' de la lista.
-int get_at (int index, void* elemento,  linked_list* list);
+int get_at (linked_list* list, int index , void* elemento);
+//Cambia el contenido del nodo en posición index por el que se le dé en element
+int owrite_at(linked_list* list, int index, void* elemento);
 //Devuelve el tamaño de la lista en número de elementos.
 int get_size(linked_list* list);
